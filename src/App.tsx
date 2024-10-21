23057235c750daaff9206c0d071b08fcc4b39248
Elysande <sirinan@hotmail.fr>
2024-10-21 12:03:33 +0200

    Update with buttons and more pokemons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import PokemonCard from "./components/PokemonCard";
 import "./App.css";
+import { useState } from "react";
 
 const pokemonList = [
   {
@@ -8,16 +9,50 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
 
 function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
   return (
     <>
       <div>
-        <PokemonCard pokemon={pokemonList[0]} />
+        <PokemonCard pokemon={pokemonList[pokemonIndex]} />
       </div>
+      <button
+        type="button"
+        onClick={() => setPokemonIndex(pokemonIndex - 1)}
+        style={{ visibility: pokemonIndex === 0 ? "hidden" : "visible" }}
+      >
+        Précédent
+      </button>
+      <button
+        type="button"
+        onClick={() => setPokemonIndex(pokemonIndex + 1)}
+        style={{
+          visibility:
+            pokemonIndex === pokemonList.length - 1 ? "hidden" : "visible",
+        }}
+      >
+        Suivant
+      </button>
     </>
   );
 }
